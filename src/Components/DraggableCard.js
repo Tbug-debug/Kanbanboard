25a37b3ea24c1c110b38a0eff9f1a0fcfea0234c
HAN LEE <lhg6325@gmail.com>
2022-08-16 17:47:10 +0900

    KanbanBoard

--- src/Components/DraggableCard.js
+++ src/Components/DraggableCard.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Draggable } from "react-beautiful-dnd";
+import { useRecoilState } from "recoil";
 import styled from "styled-components";
+import { toDoState } from "../atom";
 
 const Card = styled.div`
   border-radius: 5px;
@@ -12,7 +14,26 @@ const Card = styled.div`
     props.isDragging ? "0px 2px 5px rgba(0, 0, 0, 0.5)" : "none"};
 `;
 
+const Btn = styled.button`
+  border: none;
+  cursor: pointer;
+`;
+
 function DraggableCard({ toDoId, toDoText, index }) {
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const onClick = (event) => {
+    const targetCategory =
+      event.target.parentNode.parentNode.parentNode.firstChild.innerText;
+    setToDos((prev) => {
+      const newArray = [...prev[targetCategory]];
+      const targetIndex = newArray.findIndex((todo) => todo.id === toDoId);
+      newArray.splice(targetIndex, 1);
+      return {
+        ...prev,
+        [targetCategory]: newArray,
+      };
+    });
+  };
   return (
     <Draggable draggableId={toDoId + ""} index={index}>
       {(magic, snapshot) => (
@@ -23,6 +44,7 @@ function DraggableCard({ toDoId, toDoText, index }) {
           {...magic.dragHandleProps}
         >
           {toDoText}
+          <Btn onClick={onClick}>💨</Btn>
         </Card>
       )}
     </Draggable>
